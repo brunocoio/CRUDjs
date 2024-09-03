72e90dbb701cdc735b3877fb8270cfa3b6921c77
Bruno Depieri Barduchi <3717722+brunocoio@users.noreply.github.com>
2024-09-03 11:42:39 -0300

    Add files via upload
    https://www.youtube.com/watch?v=r7JIYv5km2U&list=PLMbclvogjXZVxxN06cefN7q5nyt7u5dUq&index=29

---
+++ firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBc_BNyXeVTjDLvJeXGonK0kuV0XWQGAZQ",
+    authDomain: "controle-ab57e.firebaseapp.com",
+    projectId: "controle-ab57e",
+    storageBucket: "controle-ab57e.appspot.com",
+    messagingSenderId: "647487351106",
+    appId: "1:647487351106:web:68be32238c6b4ce35e00db"
+};
+firebase.initializeApp(firebaseConfig);
